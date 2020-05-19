a6d76f45e6cc51b1d5c7b18e898cd557d074b808
Joaquin Fernandez <copako12@gmail.com>
2020-05-19 18:48:58 -0300

    fixed readme

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+//Sass
+import "../styles/main.scss";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Helo im quiz city :D</h1>
+    </div>
+  );
+};
+
+export default App;
